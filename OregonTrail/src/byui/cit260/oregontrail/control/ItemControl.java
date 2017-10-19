6332c8d7961181f816ff54ebd898211abfa839cf
Ralph Borcherds <ralph@mrbconsulting.biz>
2017-10-19 11:24:40 -0700

    Control Layer Class Creation

--- OregonTrail/src/byui/cit260/oregontrail/control/ItemControl.java
+++ OregonTrail/src/byui/cit260/oregontrail/control/ItemControl.java
@@ -6,7 +6,6 @@
 package byui.cit260.oregontrail.control;
 
 import byui.cit260.oregontrail.model.Item;
-import java.util.Random;
 
 /**
  *
@@ -17,12 +16,16 @@ public class ItemControl {
      
     public double calcVolumeOfBarrel(double height, double centerDiameter, double topDiameter ){
     
-        if(height <= 0) { // height is negative?
+        if(height <= 0 || height >= 35) { // height is out of range
             return -1;
         }
-        if(centerDiameter <= 0 || centerDiameter > 36) { // diameter is out of range
+        if(centerDiameter <= 0 || centerDiameter > 36) { // Center diameter is out of range
             return -1;
         }
+        if (topDiameter <= 0 || topDiameter > 30) { // Top or Bottom diameter is out of range
+            return -1;
+        }
+        
         double radius = centerDiameter / 2;
         double middradius = topDiameter / 2;
         double volume = ((Math.PI * height)/ 12) * (2 * Math.pow(middradius,2) + Math.pow(radius, 2));
@@ -32,6 +35,16 @@ public class ItemControl {
     
     public double calcWeightOfItem(int qty, String itemtype) {
         Item item = new Item();
+        
+        // qty cannot be 0
+        if (qty <= 0 ) {
+            return -1;
+        }
+        
+        // Itemtype cannot be null.   There has to be a value
+        if(itemtype == null ||  itemtype.isEmpty()){
+            return -1;
+        } 
         
         double itemWeight = item.getItemweight();
         
@@ -45,11 +58,31 @@ public class ItemControl {
         // 1 US Gallon = 231 Cubic Inches
         // 231 * .00432899 = 0.99999669 gallons - Excact Answer.
         
+        // This sets the min allowed to be 1/4 gallon of liquid
+        if (barrelvolume <= 56) {
+            return -1;
+        }
+        
+        // Max galls is 45.  10395 cubic inches is 45 gallons.
+        if (barrelvolume >= 10396) {
+            return -1;
+        }
         double gallons = barrelvolume / 231;
         return gallons;
     }
     
     public double calcCylinderVolume(double diameter, double height){
+        
+        // The diameter and height of the cylender needs to be greater than 1
+        if (diameter <= 1 || height <= 1) {
+            return -1;
+        }
+        
+        // Set the max diameter and height
+        if (diameter >= 23 || height >= 33) {
+            return -1;
+        }
+        
         double radius = diameter / 2;
         double volume = Math.pow(radius, 2) * height * Math.PI;
         return volume;

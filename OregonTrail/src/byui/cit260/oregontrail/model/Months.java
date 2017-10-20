14d6cce3fd49608d08e408d5cf7c414f160c4a29
Ralph Borcherds <ralph@mrbconsulting.biz>
2017-10-19 20:11:02 -0700

    Items Enum Added

---
+++ OregonTrail/src/byui/cit260/oregontrail/model/Months.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.oregontrail.model;
+
+/**
+ *
+ * @author ralphb
+ */
+public enum Months {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December,
+
+}

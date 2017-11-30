1d3cae7a8c84a02c4c3e4e5ebabcd53172e283c2
Ralph Borcherds <ralph@mrbconsulting.biz>
2017-11-29 17:03:52 -0800

    Week 12 commit 3

--- OregonTrail/src/byui/cit260/oregontrail/view/View.java
+++ OregonTrail/src/byui/cit260/oregontrail/view/View.java
@@ -5,81 +5,91 @@
  */
 package byui.cit260.oregontrail.view;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author ralphb
  */
 public abstract class View implements ViewInterface {
-    
-    protected String displayMessage;
 
-    public View() {
-    }
+     protected String displayMessage;
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+     public View() {
+     }
 
-    public View(String message) {
-        this.displayMessage = message;
-    }
-    
-    @Override
-    public void display() {
-        
-        boolean done = false;
-        do { 
-            // prompt for and get players name
-            String value = this.getInput();
-            if (value.toUpperCase().equals("Q")) // user wants to quit
-                return; // exit the view
+     public View(String message) {
+          this.displayMessage = message;
+      }
+     
+     
+     @Override
+     public void display() {
 
-            // do the requested action and display the next view
-            done = this.doAction(value); 
+          boolean done = false;
+          do {
+               // prompt for and get players name
+               String value = this.getInput();
+               if (value.toUpperCase().equals("Q")) // user wants to quit
+               {
+                    return; // exit the view
+               }
+               // do the requested action and display the next view
+               done = this.doAction(value);
 
-        } while (!done); // exit the view when done == true
+          } while (!done); // exit the view when done == true
 
-    }
-    
-    @Override
-    public String getInput() {
+     }
 
-        boolean valid = false;
-        String value = null;
+     @Override
+     public String getInput() {
 
-        // while a valid name has not been retrieved
-        while (!valid) {
+          boolean valid = false;
+          String value = null;
 
-            // display the prompt message
-            System.out.println("\n" + this.displayMessage);
+               // while a valid name has not been retrieved
+               while (!valid) {
 
-            // get the value entered from the keyboard
-            value = this.keyboard.readLine();
-            value = value.trim();
+               try {
+                    // display the prompt message
+                    System.out.println("\n" + this.displayMessage);
 
-            if (value.length() < 1) { // blank value entered
-                System.out.println("\n*** You must enter a value *** ");
-                continue;
-            }
-            
-            break;
-        }
+                    // get the value entered from the keyboard
+                    value = this.br.readLine();
+                    value = value.trim();
 
-        return value; // return the name        
-    }
-    
-    
-    public void clearConsole() {
-        try {
-            final String os = System.getProperty("os.name");
+                    if (value.length() < 1) { // blank value entered
+                         System.out.println("\n*** You must enter a value *** ");
+                         continue;
+                    }
 
-            if (os.contains("Windows")) {
-                Runtime.getRuntime().exec("cls");
-            } else {
-                Runtime.getRuntime().exec("clear");
-            }
-        } catch (final Exception e) {
-            //  Handle any exceptions.
-        }
+                    break;
+               } catch (IOException ex) {
+                    Logger.getLogger(View.class.getName()).log(Level.SEVERE, null, ex);
+               }
+               }
+          
 
-    }
+          return value; // return the name        
+     }
 
-   
+     public void clearConsole() {
+          try {
+               final String os = System.getProperty("os.name");
+
+               if (os.contains("Windows")) {
+                    Runtime.getRuntime().exec("cls");
+               } else {
+                    Runtime.getRuntime().exec("clear");
+               }
+          } catch (final Exception e) {
+               //  Handle any exceptions.
+          }
+
+     }
+
 }

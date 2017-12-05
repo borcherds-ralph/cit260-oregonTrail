185194e63bee64c7e1bd5d22c7d77c830eab6543
Ralph Borcherds <ralph@mrbconsulting.biz>
2017-12-04 19:19:24 -0800

    Minor updates to ENUMS

--- OregonTrail/src/byui/cit260/oregontrail/enums/Actorsenum.java
+++ OregonTrail/src/byui/cit260/oregontrail/enums/Actorsenum.java
@@ -10,18 +10,20 @@ import java.io.Serializable;
  *   @author Br33h3rr3ra
 
  */
-public enum Actorsenum implements Serializable {
+public enum Actorsenum {
 
-        Bob_Banker("The banker has  the most money and only a spouse", "1,000", "2"),
-        Fred_Farmer("The farmer has some money but a spouse and 3 children", "500", "5"),
-        Chris_Carpenter("The carpenter has a little money, 1 spouse and 1 child", "300", "3"),;
+        Bob_Banker("Bob Banker","The banker has  the most money and only a spouse", "1,000", "2"),
+        Fred_Farmer("Fred Farmer", "The farmer has some money but a spouse and 3 children", "500", "5"),
+        Chris_Carpenter("Chris Carpenter","The carpenter has a little money, 1 spouse and 1 child", "300", "3"),;
         
+        private final String name;
         private final String description;
         private final String cash;
         private final String familyMembers;
         
-        private Actorsenum(String description, String cash, String familyMembers) {
-            this.description = description;
+        private Actorsenum(String name, String description, String cash, String familyMembers) {
+            this.name = name;
+             this.description = description;
             this.cash = cash;
             this.familyMembers = familyMembers;
         }
@@ -36,6 +38,10 @@ public enum Actorsenum implements Serializable {
 
         public String getfamilyMembers() {
             return this.familyMembers;
+        }
+        
+        public String getName() {
+             return this.name;
         }
 
  }

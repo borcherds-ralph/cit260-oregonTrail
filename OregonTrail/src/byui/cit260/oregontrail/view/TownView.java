856b63e8bacb4b2c18465124b43d4920f1a1d2fd
Br33h3rr3ra <Br33h3rr3ra@Motherscomputer>
2017-12-04 21:07:32 -0600

    Lots of errors sorry professor

---
+++ OregonTrail/src/byui/cit260/oregontrail/view/TownView.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.oregontrail.view;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import byui.cit260.oregontrail.control.TownControl;
+import byui.cit260.oregontrail.model.Towns;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import oregontrail.OregonTrail;
+
+/**
+ *
+ * @author Br33h3rr3ra
+ */
+public class TownView {
+    
+}
